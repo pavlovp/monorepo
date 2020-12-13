e367f23b8ad0dcc6f79514f084866f66bf6e7b70
Pavel Pavlov <pavel.pavlov1990@gmail.com>
2020-12-13 10:21:44 +0100

    eslint fixing on save

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+
+const prettierConfig = {
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 100,
+};
+
+console.log('eslintrc!');
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: ['plugin:@typescript-eslint/recommended'], // this is optional 
+  rules: {'prettier/prettier': ['error', prettierConfig]},
+  env:{
+    node: "true"
+  }
+}
+
+/*
+const makeConfig = (isProd) => {
+    return {
+      extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended"
+      ],
+      plugins: ['@typescript-eslint', 'prettier'],
+      ignorePatterns: ['node_modules/*', 'dist/*'],
+      env: {
+        browser: true,
+        jasmine: true,
+        jest: true,
+      },
+      rules: {
+        'prettier/prettier': ['error', prettierConfig],
+        'react/prop-types': 0,
+        'no-use-before-define': 'off',
+      },
+      settings: {
+        'import/resolver': { node: { extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'] } },
+      },
+      parser: '@typescript-eslint/parser'
+    };
+  }
+  */
